2404f4769f1dc55904edbcc350ab059b1ae4ad6f
AkifHaciyev <akif6563@mail.ru>
2023-01-07 17:31:31 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import Header from "./components/Header";
 import './app.css';
+import ItemsSlider from './pages/Home/components/ItemsSlider';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<ItemsSlider />
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Home/components/ItemsSlider/index.js
+++ src/pages/Home/components/ItemsSlider/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductContext } from '../../../../context/dataContext';
+import styles from './index.module.css';
 
 const ItemsSlider = () => {
-	return <>.</>;
+	let { data } = useContext(ProductContext);
+	console.log(data);
+	return (
+		<>
+			<div className={styles.box}>
+				<div className={styles.left}>
+					<img src="" alt="" />
+				</div>
+				<div>
+					<div className="title"></div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ItemsSlider;
