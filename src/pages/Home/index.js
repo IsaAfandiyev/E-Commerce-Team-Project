9439d1192923d87c64cc971b286ec98c85a2fe4b
AkifHaciyev <akif6563@mail.ru>
2023-01-07 17:55:37 +0400

    initial commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,11 @@
 import BannerSlider from './components/BannerSlider';
 import ItemsSlider from './components/ItemsSlider';
 function HomePage() {
-<<<<<<< HEAD
 	return (
 		<>
 			<BannerSlider />
 			<ItemsSlider />
 		</>
 	);
-=======
-    return <>
-    </>
->>>>>>> develop
 }
 export default HomePage;
