d58e1eaeb83e355ee4baaf420da9d539a20a4a01
AkifHaciyev <akif6563@mail.ru>
2023-01-07 16:24:19 +0400

    add axios do ontext add canon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './app.css';
+import { ProductContext } from './context/dataContext';
+import BannerSlider from './pages/Home/components/BannerSlider';
 
 function App() {
-	return <div className="App">aaaaa</div>;
+	return (
+		<div className="App">
+			<BannerSlider />
+			<ProductContext />
+		</div>
+	);
 }
 
 export default App;
