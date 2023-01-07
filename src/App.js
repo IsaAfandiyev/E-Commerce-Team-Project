80232032ca5b5b65e73a1e7576858b265c367393
AkifHaciyev <akif6563@mail.ru>
2023-01-07 18:01:45 +0400

    error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-
-import Header from "./components/Header";
 import './app.css';
 import './app.css';
-<<<<<<< HEAD
 import PageRoutes from './Routes';
 
 function App() {
@@ -11,19 +8,6 @@ function App() {
 			<PageRoutes />
 		</>
 	);
-=======
-import ItemsSlider from './pages/Home/components/ItemsSlider';
-import PageRoutes from "./Routes";
-import HomePage from "./pages/Home/index"
-
-function App() {
-	return <>
-
-		<ItemsSlider />
-		<PageRoutes/>
-	</>
-		;
->>>>>>> develop
 }
 
 export default App;
