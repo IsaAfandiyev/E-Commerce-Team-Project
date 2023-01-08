cc1acf52ec62acc7b1da3d56cbd0db4166da9bbf
IsaAfandiyev <isa.efendiyev@gmail.com>
2023-01-08 21:07:44 +0400

    add latest news section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import styles from './index.module.css';
 import Specials from "./components/Specials";
 import Sponsors from "./components/Sponsors";
 import AboutUs from "./components/AboutUs";
+import LatestNews from "./components/LatestNews";
 
 function HomePage() {
 	return (
@@ -17,6 +18,7 @@ function HomePage() {
 			<Specials/>
 			<Sponsors/>
 			<AboutUs/>
+			<LatestNews/>
 		</div>
 	);
 }
